fd3a4cc852468cd95275ace919106b6678ac6b46
VikiRud <riablue013@gmail.com>
2023-10-13 15:34:07 +0100

    newpassword

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+let attempts = 0;
 let goodPassword = 'qwert';
-while(true) {
+while(attempts++ <3) {
     let userInput = prompt('введіть пароль');
     if(userInput === goodPassword) {
+        ok();
         break;
     }
 alert('ви ввели неправильний пароль');
 }
+console.log('ідіть геть');
+function ok () {
 console.log('це був правильний пароль');
+}
