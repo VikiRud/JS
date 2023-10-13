36cea926d73282d98abacfd4f081361a0a243600
VikiRud <riablue013@gmail.com>
2023-10-13 14:48:08 +0100

    password

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
-function month() {
-let monthNumber = Number(prompt('Ввидіть номер місяця'));
-switch (monthNumber) {
-case 1:
-case 2:
-case 12:{
-    alert('Це зима');
-    break;
+let goodPassword = 'qwert';
+while(true) {
+    let userInput = prompt('введіть пароль');
+    if(userInput === goodPassword) {
+        break;
+    }
+alert('ви ввели неправильний пароль');
 }
-case 4:
-     case 5: 
-     case 3:{
-    alert('Це весна');
-    break;
-}
-case 7: 
-case 8:
-    case 6:{
-    alert('Це літо');
-    break;
-}
-case 10:
-    case 11: 
-    case 9:{
-    alert('Це осінь');
-    break;
-}
-default: {
-    alert('Це не місяць');
-}
-}
-}
+console.log('це був правильний пароль');
