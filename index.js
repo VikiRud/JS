ae4998eb45e72197f9ae5e68c948d4d07f8c225c
VikiRud <riablue013@gmail.com>
2023-10-19 19:49:44 +0100

    zikl

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-let factNumber = 5;
-let res = 1;
-for(let counter = 1; counter <= factNumber; counter++) {
-    res *= counter;
+
+/*for(let i = 0; i <= 10; i++) 
+   if(i%2 === 0) {
+console.log(i);
+   }
+*/
+
+/*function getSumOfRange() {
+let num1 = Number(prompt('Введіть число'));
+let num2 = Number(prompt('Введіть друге число'));
+
+if (num2 > num1) {
+   let sum = 0; 
+   for (let i = num1; i <= num2; i++) {
+      sum = sum + 1;
+   }
+
+console.log(sum);
+} else {
+   console.log('Неправильні вхідні данні');
 }
+}
+*/
+/*let sqr = '';
+let num = Number(prompt('введіть розмір'));
+ 
+for (let i = 0; i < num; i++) {
+   for(let w = 0; w < num; w++) {
+      sqr += '* ';
+   }
+   sqr += '\n';
+}
+console.log(sqr);*/
+
+/*let sqr = '';
+let num = Number(prompt('введіть розмір'));
+ 
+for (let i = 0; i < num; i++) {
+   for(let w = 0; w < num; w++) {
+      if(i === 0 || i === num -1 || w === 0 || w === num -1) {
+      sqr += '* ';
+      }
+   else {
+      sqr += '  ';
+   }
+}
+   sqr += '\n';
+}
+console.log(sqr);*/
 
