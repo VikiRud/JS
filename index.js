d540b29bf0478c4696657c503ad99ee4dcf5f09a
VikiRud <riablue013@gmail.com>
2023-10-19 20:22:42 +0100

    cat

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-
-/*for(let i = 0; i <= 10; i++) 
-   if(i%2 === 0) {
-console.log(i);
-   }
-*/
-
-/*function getSumOfRange() {
-let num1 = Number(prompt('Введіть число'));
-let num2 = Number(prompt('Введіть друге число'));
-
-if (num2 > num1) {
-   let sum = 0; 
-   for (let i = num1; i <= num2; i++) {
-      sum = sum + 1;
-   }
-
-console.log(sum);
-} else {
-   console.log('Неправильні вхідні данні');
+let cat = {
+   name:'Sonik',
+   haircolor:'grey',
+   eyescolor:'blue',
+   weight:5,
+   age:7,
+   eat: function() {
+   console.log('ням-ням')},
+   run: function() {
+      console.log('тыгыдык')},
+   meow: function() {
+      console.log('мяу')},
 }
-}
-*/
-/*let sqr = '';
-let num = Number(prompt('введіть розмір'));
- 
-for (let i = 0; i < num; i++) {
-   for(let w = 0; w < num; w++) {
-      sqr += '* ';
-   }
-   sqr += '\n';
-}
-console.log(sqr);*/
-
-/*let sqr = '';
-let num = Number(prompt('введіть розмір'));
- 
-for (let i = 0; i < num; i++) {
-   for(let w = 0; w < num; w++) {
-      if(i === 0 || i === num -1 || w === 0 || w === num -1) {
-      sqr += '* ';
-      }
-   else {
-      sqr += '  ';
-   }
-}
-   sqr += '\n';
-}
-console.log(sqr);*/
-------
+console.log(cat);
+cat.age = cat.age + 1;
+console.log(cat);
+cat.favoriteFood = 'tune'
