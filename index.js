6e641c4e2c81623df105814f817220bcb77135eb
VikiRud <riablue013@gmail.com>
2023-11-01 10:52:01 +0000

    this

--- index.js
+++ index.js
@@ -1,48 +1,22 @@
-/*const user = {
-firstname: 'firstname: Jane',
-lastname: 'lastname: Doe',
-age: 'age: 18',
-email: 'email: joe@u.jdsa',
-favouritefilm: 'favouritefilm: it'
-}
-
-for (const key in user) {
-   console.log(user[key]);
-}
-
-
-function printUser(user) {
-   for (const key in user) {
-      console.log(`${key}: ${user[key]}`);
+/*const flower = {
+   type: 'rose',
+   color: 'red',
+   height: 20,
+   grow: function() {
+      this.height += 10
+      return this
    }
-}
-printUser(user)
-
-let num = {
-   a: 1,
-   b: 'hello',
-   c: 10,
-   d: 20
-}
-function getSum(num) {
-   let sum = 0;
-   for (const key in num) {
-      if(typeof num[key] === 'number') {
-         sum += num[key];
-      }
-   }
-   return sum
 }
 */
-function getPlan() {
-const weekPlan = {
-   Mon: 'go to work',
-   Tue: 'party',
-   Wed: 'dantist',
-   Thu: 'homework',
-   Fri: 'walk',
-   Sat: 'lie in bed'
-}
-   let userInput = prompt('Day of week');
-   alert(weekPlan[userInput] || 'Nothing planned')
+
+const ladder = {
+   currentStep: 0,
+   up: function() {
+      this.currentStep++;
+      return this;
+   },
+   down: function() {
+      this.currentStep--;
+      return this;
+   }
 }
