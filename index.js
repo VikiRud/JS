c7ce65a28594f3fcf707b07cbcf633578aa849f7
VikiRud <riablue013@gmail.com>
2023-11-01 09:46:58 +0000

    for...in

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-/*let computer = {
-   memory: 1024,
-   color: 'black',
-   monitor: {
-      width: {
-         value: 24,
-         uom: 'inch'
-      },
-      height: {
-         value: 30,
-         uom: 'inch'
-      },
-      manufacturer: {
-         name: 'Sony',
-         address: {
-            country: 'Japan',
-            city: 'Tokio'
-         },
-
-      },
-   },
-   on: function() {
-      console.log('This comp is on')
-   }
+const user = {
+firstname: 'firstname: Jane',
+lastname: 'lastname: Doe',
+age: 'age: 18',
+email: 'email: joe@u.jdsa',
+favouritefilm: 'favouritefilm: it'
+}
+/*
+for (const key in user) {
+   console.log(user[key]);
 }
 */
 
-/*let cup = {
-   color: 'black',
-   volum: '350ml',
-   form: 'squer',
-}
-let prop1 = 'color';
-let prop2 = 'volum';
-let prop3 = 'form';
-console.log(cup[prop2]);*/
-
-
-
-/*for (let i = 2; i <= 3; i++) {
-   for(let j =1; j<= 10; j++) {
-      console.log(`${i} * ${j} = ${i*j}`)
+function printUser(user) {
+   for (const key in user) {
+      console.log(`${key}: ${user[key]}`);
    }
 }
-*/
+printUser(user)
