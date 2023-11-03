45d95d8a93c94e283fb2d88895883b3d04239027
VikiRud <riablue013@gmail.com>
2023-11-03 20:55:25 +0000

    Array

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-function cat(name, age, color) {
-    this.name = name;
-    this.age = age;
-    this.color = color;
+/*const Array = [5, 4, 3, 2, 1];
+
+for (let i = 0; i < Array.length; i++) {
+    console.log(Array[i]);
 }
 
-function catLogic () {
-    this.eat = function() {
-        console.log(`${this.name} is eating`)
+
+function getSum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if(typeof array[i] === 'number') {
+            sum += array[i];
+        }
     }
-    this.run = function() {
-        console.log(`${this.name} is running`);
+    return sum;
+}
+
+
+
+function biggest(array) {
+    let max = array[0];
+    for (let index = 0; index < array.length; index++) {
+        if(array[index] > max) {
+            max = array[index]
+        }
     }
 }
-const CatsLogic = new catLogic();
 
-cat.prototype = CatsLogic;
+function smallest(array) {
+    let min = 0;
+    for (let index = 0; index < array.length; index++) {
+        if(arrey[index] < min) {
+            min = array[index]
+        }
+        
+    }
+}
 
+*/
+
+
+function middle(array) {
+    let sum = 0;
+    for (let index = 0; index < array.length; index++) {
+        if(typeof array[index] === 'number') {
+            sum += array[index];
+        }
+        }
+        return sum / array.length;
+    }
 
