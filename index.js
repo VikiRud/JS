81a3222c6618882c656b3640e7d683ad14e3a8c0
VikiRud <riablue013@gmail.com>
2023-11-06 09:58:41 +0000

    array metod

--- index.js
+++ index.js
@@ -36,10 +36,11 @@ function smallest(array) {
     }
 }
 
-*/
 
 
-function middle(array) {
+
+function
+ middle(array) {
     let sum = 0;
     for (let index = 0; index < array.length; index++) {
         if(typeof array[index] === 'number') {
@@ -49,3 +50,36 @@ function middle(array) {
         return sum / array.length;
     }
 
+
+
+const users = [ {
+    name: 'John',
+    lastname: 'Doe',
+    email: '123',
+}, {
+    name: 'Josh',
+    lastname: 'Joe',
+    email: '321',
+}, {
+    name: 'Jack',
+    lastname: 'Crow',
+    email: '456',
+}, {
+    name: 'Jane',
+    lastname: 'Snow',
+    email: '654',
+}
+];
+
+users.forEach(function(currentUser) {
+    console.log(currentUser.email);
+})
+
+
+const Array = [4, 5, 6, 7, 8, 9, 10];
+
+const filtered = Array.filter(function(currentValue) {
+    return currentValue %2 === 0
+}
+);
+*/
