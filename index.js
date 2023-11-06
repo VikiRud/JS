20add95fc0d5e81e39601e85f9b39da2857bb52f
VikiRud <riablue013@gmail.com>
2023-11-06 11:33:16 +0000

    tasks

--- index.js
+++ index.js
@@ -1,85 +1,59 @@
-/*const Array = [5, 4, 3, 2, 1];
+const USERNAMES = ['John', 'Josh', 'Keyli', 'Jane', 'Rick', 'Em', 'Bill', 'Mark', 'Sten'];
 
-for (let i = 0; i < Array.length; i++) {
-    console.log(Array[i]);
+function User(name, lastName, email, age, gender) {
+    this.name = name; 
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.gender = gender;
 }
 
-
-function getSum(array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        if(typeof array[i] === 'number') {
-            sum += array[i];
-        }
+function generateOneUser() {
+    const name = USERNAMES[getRandom(0, USERNAMES.length)];
+    let gender = null;
+    if(Math.random() > 0.5) {
+        gender = 'Male';
+    } else {
+        gender = 'Female';
     }
-    return sum;
+    const user = new User(name, 'Doe', `mail${getRandom(0, 100)}@mail.com}`, getRandom(10, 100), gender)
+    return user;
 }
 
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-
-function biggest(array) {
-    let max = array[0];
-    for (let index = 0; index < array.length; index++) {
-        if(array[index] > max) {
-            max = array[index]
-        }
+function getUsersArray(dimension) {
+    const arr = new Array(dimension);
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] = generateOneUser();
     }
-}
-
-function smallest(array) {
-    let min = 0;
-    for (let index = 0; index < array.length; index++) {
-        if(arrey[index] < min) {
-            min = array[index]
-        }
-        
-    }
+    return arr;
 }
 
 
+const users = getUsersArray(50);
 
 
-function
- middle(array) {
-    let sum = 0;
-    for (let index = 0; index < array.length; index++) {
-        if(typeof array[index] === 'number') {
-            sum += array[index];
-        }
-        }
-        return sum / array.length;
-    }
-
-
-
-const users = [ {
-    name: 'John',
-    lastname: 'Doe',
-    email: '123',
-}, {
-    name: 'Josh',
-    lastname: 'Joe',
-    email: '321',
-}, {
-    name: 'Jack',
-    lastname: 'Crow',
-    email: '456',
-}, {
-    name: 'Jane',
-    lastname: 'Snow',
-    email: '654',
-}
-];
-
-users.forEach(function(currentUser) {
-    console.log(currentUser.email);
+users.forEach(function(u) {
+    u.isSubscribe = Math.random() > 0.5;
 })
 
+const fullNames = users.map(function(u){
+    return `${u.name} ${u.lastName}`
+})
 
-const Array = [4, 5, 6, 7, 8, 9, 10];
+const adultUsers = users.filter(function (u) {
+    return u.age >= 18;
+})
 
-const filtered = Array.filter(function(currentValue) {
-    return currentValue %2 === 0
-}
-);
-*/
+users.sort(function(u1, u2){
+    return u1.name > u2.name ? 1 : -1;
+})
+
+const getSubscrEmails = users.map(function(u){
+    if(u.gender === 'Female' && u.isSubscribe) {
+        return u.email
+    }
+}).filter(mail => mail);
